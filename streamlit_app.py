b928a312230bab90022413817405d21923e38240
arshiya-commits <arshiyashaiik@gmail.com>
2025-09-07 22:01:36 +0530

    Update streamlit_app.py
    connected changes needed bc of move from SIS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("\U0001F379 Customize Your Smoothie! \U0001F379")
