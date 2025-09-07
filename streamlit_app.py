017a6a703f06e57c30684a805cef8fcbf788be71
arshiya-commits <arshiyashaiik@gmail.com>
2025-09-07 22:51:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session 
 
 # Write directly to the app
 st.title("\U0001F379 Customize Your Smoothie! \U0001F379")
